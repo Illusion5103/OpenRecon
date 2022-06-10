19a89b78e8cb81aa648d3374f43458a9f9209c34
nshenk <nhshenk@gmail.com>
2022-06-10 15:58:48 -0400

    Revert "init"
    This reverts commit e3008dee832655c6520fa605df0fdc93a826697d.

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -97,7 +97,7 @@ function App() {
           color: 'primary.contrastText',
           fontWeight: 'bold',
           }}>
-          Welcome to Phaeros
+          Welcome to OpenRecon
         </Typography>
         <br/>
         <Container maxWidth='md'>
@@ -105,7 +105,7 @@ function App() {
                 sx={{
                   color: '#acacad',
                   }}>
-          Open source investigative journalism with citations and peer review
+          OpenRecon is a bounty platform for open source intelligence collection powered by smart contracts and decentralized storage
         </Typography>
         </Container>
         <br/>
@@ -116,7 +116,189 @@ function App() {
             Explore Bounties
           </Button>
         </Link>
+
+        {/* {isConnected ? (
+          <Link to="/post" style={{all:"unset"}}>
+            <Button size='large' 
+              color="primary"
+              variant="outlined"
+              className="Post-Bounty">
+              Post Bounty
+            </Button>              
+          </Link>
+        ) : (
+          <Link to="/post" style={{all:"unset"}}>
+            <Button size='large'
+              color="primary"
+              variant="outlined"
+              className="Post-Bounty"
+              onClick={() => activateBrowserWallet()}>
+              Post Bounty
+            </Button>
+          </Link>
+        )} */}
+        <br/>
+        <br/>
+        <br/>
+        <br/>
         
+        <div>
+        <Card sx={{
+          backgroundColor: '#2b2b2b'
+        }}>
+          <br/>
+
+        <Box sx={{
+              flexGrow: 1,
+              fontSize: 20,
+              mx: 3,
+              textAlign: 'left',
+        }}>
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 1, sm: 1, md: 1, lg: 2, xl: 2 }}>
+            <Grid item xs={1} sm={1} md={1} lg={1} xl={1}>
+            <Link to='/explore' style={{textDecoration:"none"}}>
+            <Card sx={{
+              backgroundColor: '#212121'
+            }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  src={OR1}
+                  alt=""
+                />
+                <CardContent>
+                <Typography variant='h5' sx={{
+                  fontWeight: 'bold',
+                  color: 'white'
+                }}>
+                  <br/>
+                    Be a Collector
+                  </Typography>
+                  <br/>
+                  <Typography sx={{
+                    color: '#acacad',
+                  }}>
+                    Explore active bounties, pick one that interests you, and start collecting OSINT. 
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            </Link>
+            </Grid>
+
+            <br />
+
+            <Grid item xs={1} sm={1} md={1} lg={1} xl={1}>
+            <Link to='/post' style={{textDecoration:"none"}}>
+            <Card sx={{
+              backgroundColor: '#212121'
+            }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  src={OR2}
+                  alt=""
+                />
+                <CardContent>
+                <Typography variant='h5' sx={{
+                  fontWeight: 'bold',
+                  color: 'white'
+                }}>
+                  <br/>
+                    Be a Client
+                  </Typography>
+                  <br/>
+                  <Typography sx={{
+                    color: '#acacad',
+                  }}>
+                    Post a bounty describing what you want to know.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            </Link>
+            </Grid>
+
+
+            <Grid  item xs={1} sm={1} md={1} lg={1} xl={1}>
+          <Link to='/about' style={{textDecoration:"none"}}>
+            <Card  sx={{
+              backgroundColor: '#212121'
+            }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  src={OR3}
+                  alt=""
+                />
+                <CardContent>
+                  <Typography variant='h5' sx={{
+                    fontWeight: 'bold',
+                    color: 'white'
+                  }}>
+                    <br/>
+                    Learn How It Works
+                  </Typography>
+                  <br/>
+                  <Typography sx={{
+                    color: '#acacad',
+                  }}>
+                    Bounties are created through a smart contract on the Meter blockchain, and all bounty data storage is decentralized through IPFS/Filecoin.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            </Link>
+            </Grid>
+
+            <br />
+
+            <Grid item xs={1} sm={1} md={1} lg={1} xl={1}>
+            <Link to='/learn' style={{textDecoration:"none"}}>
+            <Card sx={{
+              backgroundColor: '#212121'
+            }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image={OR4}
+                  alt=""
+                />
+                <CardContent>
+                <Typography variant='h5' sx={{
+                  fontWeight: 'bold',
+                  color: 'white'
+                }}>
+                  <br/>
+                    Learn OSINT
+                  </Typography>
+                  <br/>
+                  <Typography sx={{
+                    color: '#acacad',
+                  }}>
+                    Learn how to collect Open Source Intelligence (OSINT) via publicly available sources and methods.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            </Link>
+            </Grid>
+
+            </Grid>
+            <br/>
+            </Box>
+            </Card>
+            </div>
+            
+            <br/>
+            <br/>
+            <br/>
+
+                  <Footer />    
     </Container>
     </Container>
     </ThemeProvider>
